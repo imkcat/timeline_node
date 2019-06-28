fe1c88ceb923fb7c521dac95d62e6e03e160b9f2
ImKcat <imkcat@icloud.com>
2019-06-28 18:14:48 +0800

    feat(Project): Added project files

---
+++ example/lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:timeline_node/timeline_node.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'timeline_node',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePageTimelineObject {
+  final TimelineNodeStyle style;
+  final String message;
+
+  HomePageTimelineObject({this.style, this.message});
+}
+
+class HomePage extends StatefulWidget {
+  List<HomePageTimelineObject> timelineObject = [
+    HomePageTimelineObject(
+        message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        style: TimelineNodeStyle(
+            pointType: TimelineNodePointType.Circle,
+            lineType: TimelineNodeLineType.BottomHalf)),
+    HomePageTimelineObject(
+        message:
+            'Maecenas non bibendum ante, et volutpat diam. Praesent eu interdum urna. Ut rhoncus, lectus sed condimentum dictum, dolor odio accumsan risus, eget malesuada enim lectus ut diam.',
+        style: TimelineNodeStyle(
+            pointType: TimelineNodePointType.Circle,
+            pointColor: Colors.red,
+            lineType: TimelineNodeLineType.Full,
+            lineColor: Colors.red)),
+    HomePageTimelineObject(
+        message:
+            'Nullam tincidunt nunc libero, vel efficitur ex elementum quis. Nam non ex laoreet, imperdiet tellus vitae, finibus augue.',
+        style: TimelineNodeStyle(
+            pointType: TimelineNodePointType.Circle,
+            pointColor: Colors.green,
+            lineType: TimelineNodeLineType.TopHalf,
+            lineColor: Colors.green)),
+    HomePageTimelineObject(
+        message:
+            'Ut fringilla dui posuere, lobortis mauris vitae, pretium ipsum.',
+        style: TimelineNodeStyle(
+            pointType: TimelineNodePointType.Circle,
+            pointRadius: 10,
+            lineType: TimelineNodeLineType.BottomHalf,
+            lineWidth: 5)),
+    HomePageTimelineObject(
+        message:
+            'Ut fringilla dui posuere, lobortis mauris vitae, pretium ipsum.',
+        style: TimelineNodeStyle(
+            lineType: TimelineNodeLineType.Full, lineWidth: 5)),
+    HomePageTimelineObject(
+        message:
+            'Praesent nulla sapien, vestibulum eget tincidunt ac, suscipit eget libero. Aliquam finibus sed nisi sed ornare.',
+        style: TimelineNodeStyle(
+            pointType: TimelineNodePointType.Circle,
+            pointRadius: 10,
+            lineType: TimelineNodeLineType.TopHalf,
+            lineWidth: 5)),
+  ];
+
+  HomePage({Key key}) : super(key: key);
+
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Timeline Node Demo'),
+      ),
+      body: ListView.builder(
+        itemCount: this.widget.timelineObject.length,
+        itemBuilder: (context, index) {
+          return TimelineNode(
+            style: this.widget.timelineObject[index].style,
+            child: Padding(
+              padding: EdgeInsets.only(right: 8, top: 4, bottom: 4),
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text(this.widget.timelineObject[index].message),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/timeline_node.dart
@@ -0,0 +1,101 @@
+library timeline_node;
+
+import 'package:flutter/material.dart';
+
+enum TimelineNodePointType { None, Circle }
+enum TimelineNodeLineType { None, Full, TopHalf, BottomHalf }
+
+class TimelineNodeStyle {
+  TimelineNodePointType pointType;
+  Color pointColor;
+  double pointRadius;
+  TimelineNodeLineType lineType;
+  Color lineColor;
+  double lineWidth;
+  double preferredWidth;
+
+  TimelineNodeStyle(
+      {this.pointType = TimelineNodePointType.None,
+      this.pointColor = Colors.blue,
+      this.pointRadius = 6,
+      this.lineType = TimelineNodeLineType.None,
+      this.lineColor = Colors.blue,
+      this.lineWidth = 2,
+      this.preferredWidth = 50});
+}
+
+class TimelineNode extends StatefulWidget {
+  final TimelineNodeStyle style;
+  final Widget child;
+
+  TimelineNode({Key key, this.style, this.child}) : super(key: key);
+
+  _TimelineNodeState createState() => _TimelineNodeState();
+}
+
+class _TimelineNodeState extends State<TimelineNode> {
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicHeight(
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: this.widget.style.preferredWidth,
+            height: double.infinity,
+            child: CustomPaint(
+              painter: TimelineNodeLinePainter(style: this.widget.style),
+            ),
+          ),
+          Expanded(child: this.widget.child)
+        ],
+      ),
+    );
+  }
+}
+
+class TimelineNodeLinePainter extends CustomPainter {
+  TimelineNodeStyle style;
+
+  TimelineNodeLinePainter({this.style});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint linePaint = Paint();
+    linePaint.color = this.style.lineColor;
+    linePaint.strokeWidth = this.style.lineWidth;
+    switch (this.style.lineType) {
+      case TimelineNodeLineType.None:
+        break;
+      case TimelineNodeLineType.Full:
+        canvas.drawLine(Offset(size.width / 2, 0),
+            Offset(size.width / 2, size.height), linePaint);
+        break;
+      case TimelineNodeLineType.TopHalf:
+        canvas.drawLine(Offset(size.width / 2, 0),
+            Offset(size.width / 2, size.height / 2), linePaint);
+        break;
+      case TimelineNodeLineType.BottomHalf:
+        canvas.drawLine(Offset(size.width / 2, size.height / 2),
+            Offset(size.width / 2, size.height), linePaint);
+        break;
+    }
+
+    Paint pointPaint = Paint();
+    pointPaint.color = this.style.pointColor;
+
+    switch (this.style.pointType) {
+      case TimelineNodePointType.None:
+        break;
+      case TimelineNodePointType.Circle:
+        canvas.drawCircle(Offset(size.width / 2, size.height / 2),
+            this.style.pointRadius, pointPaint);
+        break;
+    }
+  }
+
+  @override
+  bool shouldRepaint(TimelineNodeLinePainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(TimelineNodeLinePainter oldDelegate) => false;
+}
